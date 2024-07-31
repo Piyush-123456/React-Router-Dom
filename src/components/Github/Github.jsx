f14e35df3e2bfb4aa82e4058c0c7911ac79f39ce
PIYUSH PANCHABHAI <piyushpanchabhai200@gmail.com>
2024-07-31 13:09:18 +0530

    Loader Hook usage

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,30 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Github = () => {
-    const [data, setData] = useState(null);
 
-    useEffect(() => {
-        fetch('https://api.github.com/users/Piyush-123456')
-            .then(response => response.json())  // Parse response to JSON
-            .then(data => {
-                console.log(data);
-                setData(data);  // Set parsed data
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }, []);  // Empty dependency array to run only once
+    const data = useLoaderData()
+
 
     return (
         <center>
-            {data ? <img src={data.avatar_url} alt="Github Avatar" /> : <p>Loading...</p>}
+            <img src={data.avatar_url} alt="Github Avatar" />
         </center>
     );
 }
 
 export default Github;
 
-// export const githubInfo = async () => {
-//     const data = fetch('https://api.github.com/users/Piyush-123456')
-//     return data.json();
-// }
+export const githubInfo = async () => {
+    const data = await fetch('https://api.github.com/users/Piyush-123456')
+    return data.json();
+}
