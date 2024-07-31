47b2292cc5e7c05fdfb780f8f6e00f209eea2462
PIYUSH PANCHABHAI <piyushpanchabhai200@gmail.com>
2024-07-31 12:34:28 +0530

    Second Method of using react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -3,27 +3,36 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import "bootstrap/dist/css/bootstrap.min.css"
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Layout from '../Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout></Layout>,
-    children: [
-      {
-        path: "",
-        element: <Home></Home>
-      },
-      {
-        path: "about",
-        element: <About></About>
-      }
-    ]
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout></Layout>,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home></Home>
+//       },
+//       {
+//         path: "about",
+//         element: <About></About>
+//       }
+//     ]
+//   }
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout></Layout>}>
+      <Route path='' element={<Home />}></Route>
+      <Route path='about' element={<About />}></Route>
+    </Route>
+  )
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
